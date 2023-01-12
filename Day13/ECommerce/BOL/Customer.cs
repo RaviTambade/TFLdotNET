b40b9010b8ed5f326788f254c2c8b6f7bd2cab09
unknown <ravi.tambade@transflower.in>
2023-01-12 09:13:17 +0530

    Dashboard

---
+++ Day13/ECommerce/BOL/Customer.cs
@@ -0,0 +1,7 @@
+namespace BOL;
+public class Customer{
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public int Age{get;set;}
+}
