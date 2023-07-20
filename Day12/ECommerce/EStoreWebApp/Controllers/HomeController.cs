19a852ef666fdab4a25cd1ad1c1e89c808bdd351
Ravi Tambade <ravi.tambade@transflower.in>
2023-07-20 07:11:43 +0530

    ViewData  ViewBag TempData

--- Day12/ECommerce/EStoreWebApp/Controllers/HomeController.cs
+++ Day12/ECommerce/EStoreWebApp/Controllers/HomeController.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Mvc;
 using EStoreWebApp.Models;
 namespace EStoreWebApp.Controllers;
-
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
@@ -31,12 +30,13 @@ public class HomeController : Controller
         */
         List<Trainer> trainers=new List<Trainer>();
         trainers.Add(new Trainer () {   FirstName="Ravi",LastName="Tambade",Email="ravi.tambade@transflower.in"} );
-        trainers.Add(new Trainer () {   FirstName="Amit",LastName="Kedkar",Email="amit.khedkar@contoso.in"} );
+        trainers.Add(new Trainer () {   FirstName="Amit",LastName="Khedkar",Email="amit.khedkar@contoso.in"} );
         return Json(trainers);
         //return View();
     }
 
     public IActionResult Aboutus(){
+        ViewData["company"] = "Transflower Learning Pvt. Ltd.";
         return View();
     }
 
@@ -51,6 +51,43 @@ public class HomeController : Controller
         return View();
     }
 
+
+
+    public ActionResult List()
+    {
+        TempData["welcome"] = "Welcome to Masters";
+        return RedirectToAction("Details");
+    }
+
+
+    public ActionResult Details()
+    {
+        var someName = TempData["welcome"];
+        return View();
+    }
+
+
+
+
+    public ActionResult Combo()
+        {
+            var featuredProduct = new Product
+            {
+                Name = "Special Cupcake Assortment!",
+                Description = "Delectable vanilla and chocolate cupcakes",
+                CreationDate = DateTime.Today,
+                ExpirationDate = DateTime.Today.AddDays(7),
+                ImageName = "cupcakes.jpg",
+                Price = 5.99M,
+                QtyOnHand = 12
+            };
+
+            ViewData["FeaturedProduct"] = featuredProduct;
+            ViewBag.Product = featuredProduct;
+            TempData["FeaturedProduct"] = featuredProduct;  
+
+            return View();
+        }
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
