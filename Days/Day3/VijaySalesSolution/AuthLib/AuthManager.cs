54d86f2acc05a866d934ff4566a64aa7647ef20b
Ravi Tambade <ravi.tambade@transflower.in>
2024-05-10 16:21:58 +0530

    Vijaysales Solution

---
+++ Days/Day3/VijaySalesSolution/AuthLib/AuthManager.cs
@@ -0,0 +1,22 @@
+﻿namespace AuthLib
+{
+
+    //Business Logic for Authentication
+    public class AuthManager
+    {
+
+        public bool Login(string username, string password)
+        {
+            bool status = false;
+            if(username== "ravi" &&  password=="ravi")
+            status= true;
+            return status;
+        }
+        public bool Register(string username, string password, string email, string contact)
+        {
+            //
+            return false;
+        }
+
+    }
+}
