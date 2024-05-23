2f5c31d7a4052835774133c64ec814862d2b4b4d
Nayankothawade <nayankothawade133@gmail.com>
2024-05-23 22:57:25 +0530

    Function Call in BlogPost

--- Solutions/VijaySalesSolution/BolgPost/Repositories/BlogRepository.cs
+++ Solutions/VijaySalesSolution/BolgPost/Repositories/BlogRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using BolgPost.Entities;
 using BolgPost.DBContext;
 
