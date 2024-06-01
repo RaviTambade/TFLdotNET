99f121448f140bddd95b938761d2e5d248ef2bf8
Ravi Tambade <ravi.tambade@transflower.in>
2024-06-01 13:16:29 +0530

    MVC controller with Dependency injection

---
+++ Solutions/ECommerceSolution/ECommerceSolution/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using ECommerceSolution.Models;
+using ECommerceSolution.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceSolution.Controllers
+{
+    public class ProductsController : Controller
+    {
+
+        private IProductService _productService;
+        public ProductsController(IProductService service) {
+            _productService = service;
+        }
+
+        //CRUD Operation Action Methods
+        public IActionResult Index()
+        {
+            List<Product> products = _productService.GetAll();
+            ViewData["products"]=products;
+            return View();
+        }
+        public IActionResult Details(int id)
+        {
+            Product product = _productService.GetById(id);
+            ViewData["product"]=product;
+            return View();
+        }
+        public IActionResult Remove(int id)
+        {
+            _productService.Remove(id);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Update(int id, Product product)
+        {
+            _productService.Update(product);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Solutions/ECommerceSolution/ECommerceSolution/Services/ProductService.cs
@@ -0,0 +1,53 @@
+﻿using ECommerceSolution.Models;
+
+namespace ECommerceSolution.Services
+{
+    public class ProductService : IProductService
+    {
+
+        //data manipulation logic
+        private List<Product> products;
+
+        public ProductService()
+        {
+            products = new List<Product>();
+            products.Add(new Product { Id = 12, Name = "Laptop", Price = 30000 });
+            products.Add(new Product { Id = 13, Name = "Mobile Phone", Price = 38000 });
+            products.Add(new Product { Id = 14, Name = "HeadPhone", Price = 3000 });
+            products.Add(new Product { Id = 15, Name = "Desktop", Price = 15000 });
+        }
+    
+        public List<Product> GetAll()
+        {
+             return products;
+        }
+
+        public Product GetById(int id)
+        {
+            return products.Find(p => p.Id == id);
+        }
+
+        public void Insert(Product product)
+        {
+             products.Add(product);
+        }
+
+        public void Remove(int id)
+        {
+            var existingProduct = products.FirstOrDefault(p => p.Id == id);
+            if (existingProduct != null)
+            {
+                products.Remove(existingProduct);
+            }
+        }
+
+        public void Update(Product product)
+        {
+            var existingProduct = products.FirstOrDefault(p => p.Id == product.Id);
+            if (existingProduct != null)
+            {
+              products[products.IndexOf(existingProduct)] = product;
+            }
+        }
+    }
+}
