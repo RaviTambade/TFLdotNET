f142cd0c212fd5d8f4c604264ef8e86f76163229
Ravi Tambade <ravi.tambade@transflower.in>
2024-06-07 14:36:03 +0530

    multiple checkbox

---
+++ Solutions/BloggerSolution/StateManagmentApp/StateManagmentApp/Controllers/CustomersController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using StateManagmentApp.Models;
+
+namespace StateManagmentApp.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            Customer newCustomer = new Customer();
+            newCustomer.OrgList = PopulateOrgs();
+            return View(newCustomer);
+        }
+
+        [HttpPost]
+        public IActionResult Register(Customer theCustomer, string[] organizations )
+        {
+            theCustomer.OrgList = PopulateOrgs();
+            foreach (SelectListItem item in theCustomer.OrgList)
+            {
+                if (item != null)
+                {
+                    if (organizations.Contains(item.Value))
+                    {
+                        item.Selected = true;
+                    }
+                }
+            }
+            //send complete theCustomer to database using Entity Frameowk or database connectivity or 
+            //by using 
+            return RedirectToAction("Index");
+        }
+
+        public static List<SelectListItem> PopulateOrgs()
+        {  
+            List<SelectListItem> items = new List<SelectListItem>();
+            items.Add(new SelectListItem{ Text = "CDAC",Value = "CDAC",Selected=false});
+            items.Add(new SelectListItem { Text = "IBM", Value = "IBM", Selected = false });
+            items.Add(new SelectListItem { Text = "SEED", Value = "SEED", Selected = false });
+            items.Add(new SelectListItem { Text = "Microsoft", Value = "Microsoft", Selected = false });
+            items.Add(new SelectListItem { Text = "Google", Value = "Google", Selected = false });
+            return items;
+        }
+    }
+}
---
+++ Solutions/BloggerSolution/StateManagmentApp/StateManagmentApp/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+namespace StateManagmentApp.Models
+{
+    public class Customer
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public List<SelectListItem> OrgList { get; set; }
+
+        public Customer() { 
+                    OrgList = new List<SelectListItem>();
+        }
+
+    }
+}
